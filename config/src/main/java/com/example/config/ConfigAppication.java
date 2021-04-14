37d505886636062340e38dee272dc62677b06943
1239442470@qq.com <lzh25418>
2021-04-14 10:54:14 +0800

    第一次提交

---
+++ config/src/main/java/com/example/config/ConfigAppication.java
@@ -0,0 +1,16 @@
+package com.example.config;
+
+import com.alibaba.nacos.api.annotation.NacosProperties;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+/**
+ * 描述
+ *
+ * @author lizihao
+ * @date 2021年04月12日 15:32
+ */
+@SpringBootApplication
+@RefreshScope
+public class ConfigAppication {
+}
